75570555049873a655ddadfb2f69ffedca8d4745
Quimzy <bam.boy2020@gmail.com>
2022-03-31 22:08:34 +0300

    Fixing the import cycle between qpeer and stun

--- stun/utils.go
+++ stun/utils.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"encoding/json"
 	"encoding/base64"
-	qpeer "github.com/Quirk-io/go-qPeer/qpeer"
 )
 
 type Endpoints struct{
@@ -22,6 +21,49 @@ type Endpoint struct{
 type RegMsg struct{
 	Msgtype string 
 	PrivateEndpoint string
+}
+
+func AES_encrypt(msg string, key string) string {
+	c, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+
+	enc_msg := gcm.Seal(nonce, nonce, []byte(msg), nil)
+	return string(enc_msg)
+}
+
+func AES_decrypt(enc_msg string, key string) string {
+	c, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(enc_msg) < nonceSize {
+		log.Fatal(err)
+	}
+
+	nonce, enc_msg := enc_msg[:nonceSize], enc_msg[nonceSize:]
+	
+	msg, err := gcm.Open(nil, []byte(nonce), []byte(enc_msg), nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	return string(msg)
 }
 
 func GetPrivateIp() string {
@@ -53,14 +95,14 @@ func ImportPrivateEndpoint(jsonified_privendpoint string) Endpoint{ //From json 
 
 func Kenc_Regmsg(AES_key string) string{ //Encrypting Regmsg with AES_key
 	jsonified_regmsg, _ := json.Marshal(Reg())
-	kenc_regmsg := base64.StdEncoding.EncodeToString([]byte(qpeer.AES_encrypt(string(jsonified_regmsg), AES_key)))
+	kenc_regmsg := base64.StdEncoding.EncodeToString([]byte(AES_encrypt(string(jsonified_regmsg), AES_key)))
 
 	return kenc_regmsg
 }
 
 func Dkenc_Regmsg(AES_key string, kenc_regmsg string) RegMsg{
 	b64dec_regmsg, _ := base64.StdEncoding.DecodeString(kenc_regmsg)
-	jsonified_regmsg := qpeer.AES_decrypt(string(b64dec_regmsg), AES_key)
+	jsonified_regmsg := AES_decrypt(string(b64dec_regmsg), AES_key)
 
 	var regmsg RegMsg
 	json.Unmarshal([]byte(jsonified_regmsg), &regmsg)
@@ -70,14 +112,14 @@ func Dkenc_Regmsg(AES_key string, kenc_regmsg string) RegMsg{
 
 func Kenc_Endpoints(AES_key string, endpoints Endpoints) string{
 	jsonified_endpoints, _ := json.Marshal(endpoints)
-	kenc_endpoints := base64.StdEncoding.EncodeToString([]byte(qpeer.AES_encrypt(string(jsonified_endpoints), AES_key)))
+	kenc_endpoints := base64.StdEncoding.EncodeToString([]byte(AES_encrypt(string(jsonified_endpoints), AES_key)))
 
 	return kenc_endpoints
 }
 
 func Dkenc_Endpoints(AES_key string, kenc_endpoints string) Endpoints{
 	b64dec_endpoints, _ := base64.StdEncoding.DecodeString(kenc_endpoints)
-	jsonified_endpoints := qpeer.AES_decrypt(string(b64dec_endpoints), AES_key)
+	jsonified_endpoints := AES_decrypt(string(b64dec_endpoints), AES_key)
 
 	var endpoints Endpoints
 	json.Unmarshal([]byte(jsonified_endpoints), &endpoints)
