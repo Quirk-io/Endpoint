f1db72ffa9cb6b5a45223b01e33751f4534d5d28
Quimzy <bam.boy2020@gmail.com>
2022-04-18 18:47:27 +0300

    GetIp() added to upnp

--- upnp/upnp.go
+++ upnp/upnp.go
@@ -2,8 +2,22 @@ package upnp
 
 import (
 	upnp "github.com/jcuga/go-upnp"
-	stun "github.com/"
-	"os"
+	stun "github.com/quirkio/Endpoint/stun"
+	"log"
 )
 
-func GetIp()
+func GetIp() string{
+	//setup router
+	u, err := upnp.Discover()
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	//get Ip
+	ip, err := u.ExternalIP()
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	return ip
+}
